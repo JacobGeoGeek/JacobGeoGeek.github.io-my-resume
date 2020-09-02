2af2ac8d7570091e86493e41529cd66cbe8f8d1a
JacobGeoGeek <jacobchaar@live.ca>
2020-09-01 22:32:04 -0400

    Updates

---
+++ precache-manifest.4c121c0e9893150d3c5efd09ac50e04e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ed154a4b1267c95e6633dd1c5af23eb",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "281a3056964f17041cc1",
+    "url": "/my-resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87cecab0f8e0839f4a5b",
+    "url": "/my-resume/static/js/2.06b2cf2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-resume/static/js/2.06b2cf2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281a3056964f17041cc1",
+    "url": "/my-resume/static/js/main.86bb42e7.chunk.js"
+  },
+  {
+    "revision": "a6efcd828d3624d74f01",
+    "url": "/my-resume/static/js/runtime-main.fb0541e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my=resume/precache-manifest.c81d628bbac0719d0f3a09fc685a6bbf.js"
+  "/my-resume/precache-manifest.4c121c0e9893150d3c5efd09ac50e04e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my=resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
