665fe4dfbcc059a360175727005c1264cae36f8b
JacobGeoGeek <jacobchaar@live.ca>
2020-09-27 14:30:00 -0400

    create MyDocument componenent with a state

---
+++ src/components/page/MyDocument.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Resume } from "../../DTO/Resume";
+import * as api from "../../api/resumeApi";
+import { General } from "../general/General";
+import "../../index.css";
+import { ResumeBody } from "../resumeBody/ResumeBody";
+
+export default class MyDocument extends React.Component<{}, Resume> {
+  state: Resume = {
+    aboutMe: [],
+    educations: [],
+    generalInformation: {
+      address: "",
+      city: "",
+      email: "",
+      firstName: "",
+      githubURL: "",
+      homePhone: "",
+      lastName: "",
+      mobilePhone: "",
+      postalCode: "",
+      province: "",
+    },
+    events: [],
+    skills: {
+      languages: [],
+      technical: {
+        office: [],
+        computerScience: {
+          frameworks: [],
+          programmingLanguages: [],
+          tools: [],
+        },
+        geomatics: { CAD: [], GIS: [], other: [] },
+      },
+    },
+    workingExperiences: [],
+  };
+
+  async componentDidMount() {
+    const dataResume = await api.getResume("EN");
+
+    this.setState({
+      generalInformation: dataResume.generalInformation,
+      educations: dataResume.education,
+      aboutMe: dataResume.aboutMe,
+      skills: dataResume.skills,
+      workingExperiences: dataResume.workingExperience,
+      events: dataResume.events,
+    });
+  }
+
+  public render() {
+    return (
+      <div className="resume-wrapper-inner mx-auto text-left bg-white shadow-lg">
+        <General
+          firstName={this.state.generalInformation?.firstName!}
+          lastName={this.state.generalInformation?.lastName!}
+          address={this.state.generalInformation?.address!}
+          city={this.state.generalInformation?.city!}
+          province={this.state.generalInformation?.province!}
+          postalCode={this.state.generalInformation?.postalCode!}
+          mobilePhone={this.state.generalInformation?.mobilePhone!}
+          homePhone={this.state.generalInformation?.homePhone!}
+          email={this.state.generalInformation?.email!}
+          githubURL={this.state.generalInformation?.githubURL!}
+        />
+        <ResumeBody
+          educations={this.state.educations!}
+          aboutMe={this.state.aboutMe!}
+          skills={this.state.skills!}
+          workingExperiences={this.state.workingExperiences!}
+          events={this.state.events!}
+        />
+      </div>
+    );
+  }
+}
