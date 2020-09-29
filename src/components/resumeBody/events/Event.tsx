d12d010910e8f5ac28c31ba7dd4afa64adc5073c
JacobGeoGeek <jacobchaar@live.ca>
2020-09-28 23:08:47 -0400

    add the Event components

---
+++ src/components/resumeBody/events/Event.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from "react";
+import { IEvent } from "../../../DTO/IEvent";
+
+export const Event: FunctionComponent<IEvent> = (props) => {
+  const description = props.description.map((item, index) => (
+    <li key={index}>{item}</li>
+  ));
+  return (
+    <article className="position-relative pb-5">
+      <div className="resume-timeline-item-header mb-2">
+        <div className="d-flex flex-column flex-md-row">
+          <h3 className="resume-position-title font-weight-bold mb-1">
+            {props.title}
+          </h3>
+          <div className="resume-company-name ml-auto">
+            {props.location.building + ", " + props.location.address}
+          </div>
+        </div>
+        <div className="resume-position-time">
+          {props.startDate + " - " + props.endDate}
+        </div>
+      </div>
+      <div className="resume-timeline-item-desc">
+        <h4 className="resume-timeline-item-desc-heading font-weight-bold">
+          Description
+        </h4>
+        <ul>{description}</ul>
+      </div>
+    </article>
+  );
+};
---
+++ src/components/resumeBody/events/Events.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent } from "react";
+import { IEvent } from "../../../DTO/IEvent";
+import { Event } from "./Event";
+export const Events: FunctionComponent<{ events: Array<IEvent> }> = (props) => {
+  const listEvents = props.events.map((item, index) => (
+    <Event
+      key={index}
+      title={item.title}
+      description={item.description}
+      location={item.location}
+      startDate={item.startDate}
+      endDate={item.endDate}
+    />
+  ));
+  return (
+    <section className="resume-section reference-section mb-5">
+      <h2 className="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">
+        Events
+      </h2>
+      <div className="resume-section-content">
+        <div className="position-relative">{listEvents}</div>
+      </div>
+    </section>
+  );
+};
