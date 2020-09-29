1cb228b15b1f9c37e653c19414182fb35149b70b
JacobGeoGeek <jacobchaar@live.ca>
2020-09-28 23:06:56 -0400

    change the name interface remove the !

--- src/components/page/MyDocument.tsx
+++ src/components/page/MyDocument.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Resume } from "../../DTO/Resume";
+import { IResume } from "../../DTO/IResume";
 import * as api from "../../api/resumeApi";
 import { General } from "../general/General";
 import "../../index.css";
 import { ResumeBody } from "../resumeBody/ResumeBody";
 
-export default class MyDocument extends React.Component<{}, Resume> {
-  state: Resume = {
+export default class MyDocument extends React.Component<{}, IResume> {
+  state: IResume = {
     aboutMe: [],
     educations: [],
     generalInformation: {
@@ -52,27 +52,29 @@ export default class MyDocument extends React.Component<{}, Resume> {
 
   public render() {
     return (
-      <div className="resume-wrapper-inner mx-auto text-left bg-white shadow-lg">
-        <General
-          firstName={this.state.generalInformation?.firstName!}
-          lastName={this.state.generalInformation?.lastName!}
-          address={this.state.generalInformation?.address!}
-          city={this.state.generalInformation?.city!}
-          province={this.state.generalInformation?.province!}
-          postalCode={this.state.generalInformation?.postalCode!}
-          mobilePhone={this.state.generalInformation?.mobilePhone!}
-          homePhone={this.state.generalInformation?.homePhone!}
-          email={this.state.generalInformation?.email!}
-          githubURL={this.state.generalInformation?.githubURL!}
-        />
-        <ResumeBody
-          educations={this.state.educations!}
-          aboutMe={this.state.aboutMe!}
-          skills={this.state.skills!}
-          workingExperiences={this.state.workingExperiences!}
-          events={this.state.events!}
-        />
-      </div>
+      <article className="resume-wrapper text-center position-relative">
+        <div className="resume-wrapper-inner mx-auto text-left bg-white shadow-lg">
+          <General
+            firstName={this.state.generalInformation.firstName}
+            lastName={this.state.generalInformation.lastName}
+            address={this.state.generalInformation.address}
+            city={this.state.generalInformation.city}
+            province={this.state.generalInformation.province}
+            postalCode={this.state.generalInformation.postalCode}
+            mobilePhone={this.state.generalInformation.mobilePhone}
+            homePhone={this.state.generalInformation.homePhone}
+            email={this.state.generalInformation.email}
+            githubURL={this.state.generalInformation.githubURL}
+          />
+          <ResumeBody
+            educations={this.state.educations}
+            aboutMe={this.state.aboutMe}
+            skills={this.state.skills}
+            workingExperiences={this.state.workingExperiences}
+            events={this.state.events}
+          />
+        </div>
+      </article>
     );
   }
 }
