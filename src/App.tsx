9e00dbcec71e2ca3602ee1a34d5a83356333a102
JacobGeoGeek <jacobchaar@live.ca>
2020-09-29 20:42:32 -0400

    transform the function into class component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,41 @@ import React from "react";
 import MyDocument from "./components/page/MyDocument";
 import "./App.css";
 import { Credit } from "./components/page/Credit";
-function App() {
-  return (
-    <div>
-      <MyDocument />
-      <Credit />
-    </div>
-  );
+import { Language } from "./components/page/Language";
+
+enum languages {
+  EN = "EN",
+  FR = "FR",
 }
 
-export default App;
+interface IAppState {
+  language: languages;
+  changeLanguage: () => void;
+}
+
+function getOppsiteLanguage(language: string): string {
+  return language === languages.EN ? languages.FR : languages.EN;
+}
+
+export default class App extends React.Component<{}, IAppState> {
+  state: IAppState = {
+    language: languages.FR,
+    changeLanguage: () =>
+      this.state.language === "EN"
+        ? this.setState({ language: languages.FR })
+        : this.setState({ language: languages.EN }),
+  };
+
+  public render() {
+    return (
+      <div>
+        <MyDocument language={getOppsiteLanguage(this.state.language)} />
+        <Credit />
+        <Language
+          language={this.state.language}
+          changeLanguage={this.state.changeLanguage}
+        />
+      </div>
+    );
+  }
+}
