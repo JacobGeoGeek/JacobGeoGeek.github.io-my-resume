1dd100535dc5501753e32f300181ef9f692e52e5
JacobGeoGeek <jacobchaar@live.ca>
2020-09-29 20:40:25 -0400

    create sub folder for about me component

--- src/components/resumeBody/aboutMe/AboutMe.tsx
+++ src/components/resumeBody/aboutMe/AboutMe.tsx
@@ -1,19 +1,19 @@
 import React, { FunctionComponent } from "react";
-import "../../index.css";
+import "../../../index.css";
 
 export const AboutMe: FunctionComponent<{ aboutMe: Array<string> }> = (
   props
 ) => {
-  const listAboutMe = props.aboutMe.map((item, index) => (
-    <li key={index}>{item}</li>
-  ));
   return (
     <section className="resume-section summary-section mb-5">
       <h2 className="resume-section-title text-uppercase font-weight-bold pb-3 mb-3">
         About Me
       </h2>
       <div className="resume-section-content">
-        <ul>{listAboutMe}</ul>
+        <p>
+          A graduated computer scientist student who is:{" "}
+          {props.aboutMe.join(" - ")}
+        </p>
       </div>
     </section>
   );
