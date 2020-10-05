702971c30b0a68ae538ce2a97efe12e4d9fe2d8a
JacobGeoGeek <jacobchaar@live.ca>
2020-10-05 14:00:55 -0400

    add condition to check if the app run in local

--- src/configuration/Configuration.ts
+++ src/configuration/Configuration.ts
@@ -1,5 +1,9 @@
 export class Configuration {
-  static API_URL = "http://localhost:3001";
+  static API_URL =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
+      ? "http://localhost:3001"
+      : "https://jacob-resume-backend.herokuapp.com";
 
   static headers = {
     method: "GET",
