0e99458d0603cf8f7eaef8f0c1c7b0ffa80be0bb
JacobGeoGeek <jacobchaar@live.ca>
2020-10-05 18:02:29 -0400

    add Cors Policy

--- src/api/resumeApi.ts
+++ src/api/resumeApi.ts
@@ -1,7 +1,7 @@
 import { Configuration } from "../configuration/Configuration";
 
 export const getResume = (language: string): any => {
-  return fetch(`${Configuration.API_URL}/${language}`, { method: "GET" })
+  return fetch(`${Configuration.API_URL}/${language}`, { method: "GET", headers: Configuration.headers })
     .then((response) => response.json())
     .then((result) => result)
     .catch((error) => console.error(error));
--- src/configuration/Configuration.ts
+++ src/configuration/Configuration.ts
@@ -1,12 +1,11 @@
 export class Configuration {
   static API_URL =
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1"
+    window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
       ? "http://localhost:3001"
       : "https://jacob-resume-backend.herokuapp.com";
 
   static headers = {
-    method: "GET",
-    redirect: "follow",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
   };
 }
