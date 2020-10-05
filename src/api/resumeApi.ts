b8ccb244b77bf7696a290313c5d63f256e6f5b56
JacobGeoGeek <jacobchaar@live.ca>
2020-10-05 18:51:03 -0400

    lowercase the get

--- src/api/resumeApi.ts
+++ src/api/resumeApi.ts
@@ -1,7 +1,7 @@
 import { Configuration } from "../configuration/Configuration";
 
 export const getResume = (language: string): any => {
-  return fetch(`${Configuration.API_URL}/${language}`, { method: "GET", headers: Configuration.headers })
+  return fetch(`${Configuration.API_URL}/${language}`, { method: "get", headers: Configuration.headers })
     .then((response) => response.json())
     .then((result) => result)
     .catch((error) => console.error(error));
