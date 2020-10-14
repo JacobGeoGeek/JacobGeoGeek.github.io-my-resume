a3cb676aa9f02c1e3530922925855369fb1049ba
JacobGeoGeek <jacobchaar@live.ca>
2020-10-13 23:13:47 -0400

    add pdf URL props

--- src/components/page/Export.tsx
+++ src/components/page/Export.tsx
@@ -3,7 +3,7 @@ import Fab from "@material-ui/core/Fab";
 import { GoDesktopDownload } from "react-icons/go"
 import { useTranslation } from "react-i18next";
 
-export const Export: FunctionComponent<{ languege: string }> = (props) => {
+export const Export: FunctionComponent<{ pdfURL: string }> = (props) => {
     const { t } = useTranslation()
     
     return (
@@ -15,8 +15,9 @@ export const Export: FunctionComponent<{ languege: string }> = (props) => {
                 left: "50px",
                 backgroundColor: "#22A162",
             }}>
-                
-            <GoDesktopDownload size="2em" style={{color:"white"}} title={t("export")} />
+            <a href={props.pdfURL} target="_blank" rel="noopener noreferrer">
+                <GoDesktopDownload size="2em" style={{ color: "white" }} title={t("export")} />
+            </a>
         </Fab>
     )
 }
