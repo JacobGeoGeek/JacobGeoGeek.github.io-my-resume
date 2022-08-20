b0dcc7b5c66a09c65fe5c6d85f9fd5c8f9a98e70
Jacob Chaar <jchaar@k2geospatial.com>
2022-08-19 21:42:57 -0400

    Remove the unused import and format code.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Language } from "./components/page/Language";
 import { Export } from "./components/page/Export";
 import { IResume } from "./DTO/IResume";
 import { FactoryResume } from "./components/page/factory/FactoryResume";
-import { Resume } from "./components/Resume";
 import * as api from "./api/resumeApi";
 import { MyDocument } from "./components/page/MyDocument";
 import { languages } from "./i18n/languages";
@@ -19,40 +18,48 @@ interface Resume {
 
 export default class App extends React.Component<{}, Resume> {
   private resumeFactory: FactoryResume;
-  
-  constructor(props={}) {
-    super(props)
+
+  constructor(props = {}) {
+    super(props);
     this.resumeFactory = new FactoryResume();
     this.state = {
       resumeData: this.resumeFactory.initializeResume(),
       language: languages.FR,
       changeLanguage: () =>
-      this.state.language === languages.EN
-        ? this.setState({ language: languages.FR })
-        : this.setState({ language: languages.EN })
-    }
-  }
-  
-  async componentDidMount() {
-    const dataResume = await api.getResume(getOppositeLanguage(this.state.language));
-    this.setState({ resumeData: this.resumeFactory.createResume(dataResume) });
-  }
-  
-  async componentDidUpdate(prevProps:{},prevState: Resume) {
-    if (this.state.language !== prevState.language) {
-      const dataResume = await api.getResume(getOppositeLanguage(this.state.language));
-      this.setState({ resumeData: this.resumeFactory.createResume(dataResume) });
-    }
+        this.state.language === languages.EN
+          ? this.setState({ language: languages.FR })
+          : this.setState({ language: languages.EN }),
+    };
   }
 
+  async componentDidMount() {
+    const dataResume = await api.getResume(
+      getOppositeLanguage(this.state.language)
+    );
+    this.setState({ resumeData: this.resumeFactory.createResume(dataResume) });
+  }
+
+  async componentDidUpdate(prevProps: {}, prevState: Resume) {
+    if (this.state.language !== prevState.language) {
+      const dataResume = await api.getResume(
+        getOppositeLanguage(this.state.language)
+      );
+      this.setState({
+        resumeData: this.resumeFactory.createResume(dataResume),
+      });
+    }
+  }
 
   public render() {
     return (
       <div>
-        <MyDocument resume={this.state.resumeData}/>
+        <MyDocument resume={this.state.resumeData} />
         <Credit />
-        <Language language={this.state.language} changeLanguage={this.state.changeLanguage} />
-        <Export pdfURL={this.state.resumeData.PDF}/>
+        <Language
+          language={this.state.language}
+          changeLanguage={this.state.changeLanguage}
+        />
+        <Export pdfURL={this.state.resumeData.PDF} />
       </div>
     );
   }
--- src/configuration/Configuration.ts
+++ src/configuration/Configuration.ts
@@ -1,6 +1,7 @@
 export class Configuration {
   static API_URL =
-    window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
       ? "http://localhost:3001"
       : "https://jacob-resume-backend.herokuapp.com";
 
